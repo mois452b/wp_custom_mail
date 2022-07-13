b72fc15f56b201e9f4b1cbee77d0dab254711821
Octavio Martinez <omartinez1618@gmail.com>
2022-07-13 17:39:39 -0400

    fc

---
+++ wp_custom_mail.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Custom Mails
+ * Plugin URI: https://kavavdigital.com
+ * Description: 
+ * Version: 6.7.0
+ * Author: Octavio Martinez
+ * Author URI: https://github.com/zenx5 
+ * 
+ */
