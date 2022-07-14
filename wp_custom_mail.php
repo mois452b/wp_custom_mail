99e7bb3ac737095d4b1b4a78893c34e114aa35c7
moises <elmoises.reydereymail.com>
2022-07-13 20:03:22 -0400

    fc

--- wp_custom_mail.php
+++ wp_custom_mail.php
@@ -8,3 +8,29 @@
  * Author URI: https://github.com/zenx5 
  * 
  */
+
+function send_mail( ) {
+	wp_mail('elmoises.reyderey@gmail.com', 'Prueba de envio de email', 'enviando un email a travez de wp_mail() de wordpress');
+}
+
+add_action( 'init', 'send_mail' );
+
+function configuracion_smtp( $phpmailer ){
+    $phpmailer->isSMTP(); 
+    $phpmailer->Host = 'smtp.gmail.com';
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->Port = 25;
+    $phpmailer->Username = 'moisesgabrielrodriguezruiz@gmail.com';
+    $phpmailer->Password = '30810280';
+    $phpmailer->SMTPSecure = false;
+    $phpmailer->From = 'moisesgabrielrodriguezruiz@gmail.com';
+    $phpmailer->FromName='Moises rodriguez';
+    $phpmailer->AddAddress='elmoises.reyderey@gmail.com';
+    $phpmailer->Subject='prueba de email';
+    $phpmailer->Body='enviando email por medio de phpmailer';
+    $phpmailer->AltBody='enviando email por medio de phpmailer';
+	
+	
+}
+
+add_action( 'phpmailer_init', 'configuracion_smtp' );
