19f7c2b4820d5fcca459cd65da9b87afd03f45ae
moises <elmoises.reydereymail.com>
2022-07-19 23:28:40 -0400

    listo setting

--- wp_custom_mail.php
+++ wp_custom_mail.php
@@ -111,21 +111,50 @@ class WP_custom_mail{
         update_option('wpcm_user', $user );
         update_option('wpcm_name', $name );
         update_option('wpcm_password', $password );
-
-        return 'listo';
     }
 
     public static function wpcm_send_mail( ){
+        $user = get_option('wpcm_user');
+        $name = get_option('wpcm_name');
+        $password = get_option('wpcm_password') ? 'nopermitidoverelpassword' : '';
+
         $mail = new wp_custom_mail( );
         try {
-            $mail->user( 'omartinez1618@gmail.com', 'omartinez1618@gmail.com' );
-            $mail->host('smtp.gmail.com');
-            $mail->port(465);
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = $user;                  //SMTP username
+            $mail->Password   = $password;   //'fhhynumuxgzfksmj';       //vsgweaowshtdgxdh        //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        
+            //Recipients
+            $mail->setFrom( $user, $name );
+            $mail->addAddress('elmoises.reyderey@gmail.com', 'Moises Rodriguez');     //Add a recipient
+            // $mail->addAddress('ellen@example.com');               //Name is optional
+            // $mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+        
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        
+            //Content
+            ob_start( );
+            include WP_PLUGIN_DIR.'/wp_custom_mail/template/index.html';
+            $body = ob_get_contents( );
+            ob_end_clean( );
+            echo $body;
 
-            $mail->to('elmoises.reyderey@gmail.com');
-            $mail->subject('hola moises');
-            $mail->body('ejemplo de boy');
-            // $mail->send( );
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Here is the subject 2';
+            $mail->Body    = $body;
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        
+            $mail->send();
 
             echo 'Message has been sent correctamente';
         } catch (Exception $e) {
