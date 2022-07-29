ee8bc649009ba25a75f3480ebdf7b07c615450b0
Octavio Martinez <omartinez1618@gmail.com>
2022-07-29 09:47:55 -0400

    quitando credenciales

--- src/class-wp-custom-mail.php
+++ src/class-wp-custom-mail.php
@@ -145,10 +145,10 @@ class Mails{
             $mail = new PHPMailer();    
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = $user;       //omartinez1618@gmail.com           //SMTP username
-            $mail->Password   = $password;   //'fhhynumuxgzfksmj';        //SMTP password
+            $mail->Username   = $user;                                  //SMTP username
+            $mail->Password   = $password;                              //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
